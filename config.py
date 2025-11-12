654391a4bd09fe8cf465267aca3e1c7309e732fd
bandsight <johns.applications@gmail.com>
2025-11-12 21:07:26 +1100

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,4 @@ DELAY_BETWEEN_COUNCILS = 3  # Seconds
 DELAY_AFTER_CLICK = 3  # Seconds for loads
 RSS_DAYS_BACK = 30  # Jobs in RSS (filter by posted_date)
 FALLBACK_COUNCILS_FILE = 'fallback_councils.json'  # Backup if directory fails
-QUICK_TEST_COUNCILS = ['City of Ballarat']  # Run only these for debug
-
+QUICK_TEST_COUNCILS = ['City of Ballarat']  # Run only these for debug (overrides TEST_MODE)
